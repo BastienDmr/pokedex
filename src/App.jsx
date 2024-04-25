609dbf85e516f6a5ee05b758234462396cab8125
BastienDmr <domer.bastien@gmail.com>
2024-04-25 15:03:13 +0200

    Fin step 6

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,49 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickUp = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+  const handleClickDown = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
   return (
     <>
       <div>
-        <PokemonCard pokemon={pokemonList[0]} />
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
       </div>
+      {pokemonIndex > 0 ? (
+        <button onClick={handleClickDown}>précedent</button>
+      ) : (
+        ""
+      )}
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={handleClickUp}>suivant</button>
+      ) : (
+        ""
+      )}
     </>
   );
 }
